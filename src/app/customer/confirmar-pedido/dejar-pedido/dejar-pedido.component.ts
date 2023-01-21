9370d0f346528cbb7525b240a5d89818179e0015
Frank Pizarro <92424277+Nemewar@users.noreply.github.com>
2023-01-21 14:32:18 -0500

    change routerlink to click forward

--- src/app/customer/confirmar-pedido/dejar-pedido/dejar-pedido.component.ts
+++ src/app/customer/confirmar-pedido/dejar-pedido/dejar-pedido.component.ts
@@ -16,23 +16,30 @@ export class DejarPedidoComponent extends ViewComponent implements OnInit {
   @Input()
   recojoTienda: boolean = false;
 
-  constructor(_injector: Injector) { 
+  constructor(_injector: Injector) {
     super(_injector)
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  goTo(){
-    this.message.confirm("Al otorgar acceso, podrás ver tus contactos"
-      , "¿Induacril quiere acceder a tus contactos?"
-      , (confirmation) => {
-        if(confirmation){
-          this.navigation.forward("/customer/contact")
-        }else{
-          this.navigation.forward("/customer/buy")
-        }
-      },"permitir", "No permitir"
+  goTo(p1?: string, p2?: string) {
+
+    if (p2) {
+      this.message.confirm("Al otorgar acceso, podrás ver tus contactos"
+        , "¿Induacril quiere acceder a tus contactos?"
+        , (confirmation) => {
+          if (confirmation) {
+            this.navigation.forward(p1)
+          } else {
+            this.navigation.forward(p2)
+          }
+        }, "permitir", "No permitir"
       )
+    }else{
+      this.navigation.forward(p1);
+    }
+
+
   }
 
 }
