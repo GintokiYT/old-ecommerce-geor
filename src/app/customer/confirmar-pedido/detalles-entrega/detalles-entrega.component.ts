c260b0e1e8043d7921e45d075fc0e784b4b0f652
Frank Pizarro <92424277+Nemewar@users.noreply.github.com>
2023-01-21 09:14:33 -0500

    update variables.scss

---
+++ src/app/customer/confirmar-pedido/detalles-entrega/detalles-entrega.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+
+interface Options{
+  envioADomicilio: boolean,
+  recojoEnTienda: boolean
+}
+
+@Component({
+  selector: 'app-detalles-entrega',
+  templateUrl: './detalles-entrega.component.html',
+  styleUrls: ['./detalles-entrega.component.scss'],
+})
+export class DetallesEntregaComponent implements OnInit {
+
+
+  @Input()
+  envioADomicilio: boolean = false;
+
+  @Input()
+  recojoTienda: boolean = false;
+
+  @Output()
+  onChangeOption: EventEmitter<Options> = new EventEmitter<Options>();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSelect(id:string){
+    const opciones = document.querySelectorAll(".selected");
+    const opcion = document.getElementById(id);
+    opciones.forEach( opc => opc.classList.remove("selected"));
+    opciones.forEach( opc => opc.classList.add("notSelected"));
+    opcion?.classList.add("selected")
+
+    if(id==="location"){
+      this.envioADomicilio = true;
+      this.recojoTienda = false;
+    }
+
+    if(id==="home"){
+      this.envioADomicilio = false;
+      this.recojoTienda = true;
+    }
+
+    const obj = {
+       envioADomicilio: this.envioADomicilio,
+       recojoEnTienda : this.recojoTienda
+    }
+
+    this.onChangeOption.emit(obj)
+  }
+
+
+
+}
--- src/app/customer/customer.routing.module.ts
+++ src/app/customer/customer.routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ConfirmarPedidoComponent } from './confirmar-pedido/confirmar-pedido.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: 'home', component: HomeComponent },
+      { path: 'confirmar-pedido', component: ConfirmarPedidoComponent },
       { path: '', pathMatch: 'full', redirectTo: 'home' },
       { path: '**', redirectTo: 'home' }
     ]
