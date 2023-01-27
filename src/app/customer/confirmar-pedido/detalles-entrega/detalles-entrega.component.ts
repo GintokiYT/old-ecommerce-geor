2493011109d21423d5bf6de7254e97a8adaa3eff
Frank Pizarro <92424277+Nemewar@users.noreply.github.com>
2023-01-27 13:56:46 -0500

    solucionado error de cambio de pestaña

--- src/app/customer/confirmar-pedido/detalles-entrega/detalles-entrega.component.ts
+++ src/app/customer/confirmar-pedido/detalles-entrega/detalles-entrega.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ConfirmarPedidoService } from '../services/confirmar-pedido.service';
 
-interface Options{
+interface Options {
   envioADomicilio: boolean,
   recojoEnTienda: boolean
 }
@@ -16,16 +16,21 @@ export class DetallesEntregaComponent implements OnInit {
 
 
   constructor(private cpService: ConfirmarPedidoService) {
+
   }
 
   ngOnInit() {
-
+    const opciones = document.querySelectorAll(".selected");
+    opciones.forEach(opc => opc.classList.remove("selected"));
+    const pedidoSeleccionado = this.cpService.currentMiPedido$.subscribe(pedido => {
+      document.getElementById(pedido.tipoPedido).classList.add("selected");
+    })
   }
 
-  onSelect(id:string){
+  onSelect(id: string) {
     const opciones = document.querySelectorAll(".selected");
     const opcion = document.getElementById(id);
-    opciones.forEach( opc => opc.classList.remove("selected"));
+    opciones.forEach(opc => opc.classList.remove("selected"));
     opcion?.classList.add("selected")
 
     this.cpService.setTipoPedido(id);
