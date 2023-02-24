ee0fae2098072406624ee95bb81a41fd1aad98cc
yanci97 <yanciv2005@gmail.com>
2023-02-24 17:21:59 -0500

    ajustes

--- src/app/customer/store-pickup/store-map/store-map.component.ts
+++ src/app/customer/store-pickup/store-map/store-map.component.ts
@@ -22,30 +22,11 @@ export class StoreMapComponent extends GeolocationComponent implements OnInit {
 
   override ngOnDestroy(): void {
     super.ngOnDestroy();
-
-    //google.maps.event.removeListener(this.mapCenterChangeListener);
-    //google.maps.event.removeListener(this.mapCenterDragEndListener);
   }
 
   override onMapLoaded(): void {
-
-    // this.positionMarker = this.googleMap.createMarker(this.map, {
-    //   latitude: this.geolocation.position.coords.latitude,
-    //   longitude: this.geolocation.position.coords.longitude
-    // });
-
-    // this.mapCenterChangeListener = google.maps.event.addListener(this.map, 'center_changed', () => {
-    //   this.positionMarker.setPosition(this.map.getCenter());
-    // });
-
-    // this.mapCenterDragEndListener = google.maps.event.addListener(this.map, 'dragend', () => {
-    //   const center: google.maps.LatLng = this.map.getCenter();
-
-    // });
   }
   goBack(){
     this.location.back();
   }
-
-
 }
