469e7dfbeb3e0970e8442c9758a7dc4a31efb499
JeysonOrellano <orellano.lopez.jeyson@gmail.com>
2023-03-02 15:38:31 -0500

    update

--- src/app/customer/basket/contact-basket/contact-basket.component.ts
+++ src/app/customer/basket/contact-basket/contact-basket.component.ts
@@ -1,7 +1,10 @@
 import { Location } from '@angular/common';
 import { Component, OnInit, Injector } from '@angular/core';
 import { ViewComponent } from '@geor360/ecore';
-
+interface Contacts{
+  name:string,
+  number:string;
+}
 @Component({
   selector: 'app-contact-basket',
   templateUrl: './contact-basket.component.html',
@@ -9,43 +12,37 @@ import { ViewComponent } from '@geor360/ecore';
 })
 export class ContactBasketComponent extends ViewComponent implements OnInit {
 
-  oneTrue:boolean=true;
   constructor( _injector: Injector, private location: Location ) {
     super(_injector);
    }
 
   ngOnInit() {}
 
-  data: any[] = [
+  contact:Contacts[]=[
     {
-      id: 0,
-      selected: false,
+      name: 'Jualiano del carmen',
+      number:'+51 971 945 234',
+
     },
     {
-      id:1,
-      selected: false,
-    },
-    {
-      id:2,
-      selected: false,
-    },
-    {
-      id:3,
-      selected: false,
-    },
-    {
-      id:4,
-      selected: false,
-    },
-    {
-      id:5,
-      selected: false,
-    },
-    {
-      id:6,
-      selected: false,
-    },
-  ]
+      name: 'Roberto Carlos de maracaná',
+      number:'+51 971 945 234',
+
+    }];;
+
+  oneTrue = false;
+  invitationsCount = 0;
+
+  checkBoxSelect(event: any) {
+    if (event.detail.checked) {
+     // console.log(event.detail.checked);
+      this.invitationsCount++;
+      this.oneTrue = true;
+    } else {
+      this.invitationsCount--;
+      this.oneTrue = this.invitationsCount > 0;
+    }
+  }
 
   goBack(){
     this.location.back();
@@ -59,16 +56,7 @@ export class ContactBasketComponent extends ViewComponent implements OnInit {
     this.navigation.root('/customer/contact-search','forward');
   }
 
-  checkBoxSelect(id: number) {
-    const falses = this.data.filter( element => element.id!==id);
-    const trues = this.data.filter(element => element.selected===true);
-    falses.forEach( element => {
-    element.selected = false;
-    })
-    if(trues.length>0){
-      this.oneTrue = false;
-    }else{
-      this.oneTrue = true;
-    }
-  }
 }
+
+
+
