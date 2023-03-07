49987af6b1af6e9732e9d57d783a10783c1e519d
Frank Pizarro <92424277+Nemewar@users.noreply.github.com>
2023-03-07 14:35:59 -0500

    finish manage billing data

--- src/app/customer/manage-billing-data/bills-delete/bills-delete.component.ts
+++ src/app/customer/manage-billing-data/bills-delete/bills-delete.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { BillingDataService } from '../../../services/billing-data.service';
+import { ViewComponent } from '@geor360/ecore';
 
 @Component({
   selector: 'app-bills-delete',
   templateUrl: './bills-delete.component.html',
   styleUrls: ['./bills-delete.component.scss'],
 })
-export class BillsDeleteComponent implements OnInit {
+export class BillsDeleteComponent extends ViewComponent implements OnInit {
 
   billingData : any[];
   oneTrue: boolean = false;
   modalIsVisible: boolean = false;
 
-  constructor(private bds: BillingDataService) {
+  constructor(private bds: BillingDataService, private _injector: Injector) {
+    super(_injector);
     this.bds.currentBillingData$.subscribe( (data) => {
       this.billingData = data;
     })
@@ -59,5 +61,10 @@ export class BillsDeleteComponent implements OnInit {
       this.modalIsVisible = value;
     }
   }
+
+  goToEdit(){
+    this.navigation.forward("/customer/manage-billing-data/add-company")
+  }
+
 
 }
