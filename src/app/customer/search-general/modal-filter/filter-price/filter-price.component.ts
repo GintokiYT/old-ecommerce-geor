05f310c218314142dbc22caba84ed6b33c5d58db
Renato <satoshiune21@gmail.com>
2023-03-15 13:24:24 -0500

    input range 100%

--- src/app/customer/search-general/modal-filter/filter-price/filter-price.component.ts
+++ src/app/customer/search-general/modal-filter/filter-price/filter-price.component.ts
@@ -9,11 +9,6 @@ export class FilterPriceComponent implements OnInit {
 
   @ViewChild('myPrice') myPrice: ElementRef;
 
-  @ViewChild('slider') slider: ElementRef;
-  @ViewChild('selector') selector: ElementRef;
-  @ViewChild('selectValue') selectValue: ElementRef;
-  @ViewChild('progressBar') progressBar: ElementRef;
-
   statusArrow: boolean;
 
   constructor() { }
@@ -21,22 +16,83 @@ export class FilterPriceComponent implements OnInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    const slider: HTMLInputElement = this.slider.nativeElement;
-    const selector: HTMLDivElement = this.selector.nativeElement;
-    const selectValue: HTMLDivElement = this.selectValue.nativeElement;
-    const progressBar: HTMLDivElement = this.progressBar.nativeElement;
+    const slider: HTMLDivElement = document.querySelector('.slider');
+    const sliderFill: HTMLDivElement = document.querySelector('.slider-fill');
+    const sliderThumb: HTMLDivElement = document.querySelector('.slider-thumb');
+    const output: HTMLDivElement = document.querySelector('.output');
 
-    selectValue.textContent = String(Number(slider.value) === 0 ? 0 : Number(slider.value).toFixed(2));
+    let isDragging = false;
+    let sliderValue: any = 30;
 
-    selector.style.left = `${slider.value}%`;
+    updateSlider(sliderValue);
 
-    progressBar.style.width = `${slider.value}%`;
-
-    slider.addEventListener('input', () => {
-      selector.style.left = slider.value + '%';
-      selectValue.textContent = String(Number(slider.value) === 0 ? 0 : Number(slider.value).toFixed(2));
-      progressBar.style.width = `${slider.value}%`;
+    // PC
+    slider.addEventListener('mousedown', () => {
+      isDragging = true;
     });
+    // Celular
+    slider.addEventListener('touchstart', (event) => {
+      event.preventDefault();
+      isDragging = true;
+    });
+
+    // PC
+    slider.addEventListener('mouseup', () => {
+      isDragging = false;
+    });
+    // Celular
+    slider.addEventListener('touchend', () => {
+      isDragging = false;
+    });
+
+    // PC
+    slider.addEventListener('mousemove', (event) => {
+      if (isDragging) {
+        let mousePosition: any = getMousePosition(event);
+        let sliderPosition: any = getSliderPosition(mousePosition.x);
+        updateSlider(sliderPosition);
+      }
+    });
+    // Celular
+    slider.addEventListener('touchmove', (event) => {
+      event.preventDefault();
+      if (isDragging) {
+        let touchPosition: any = getTouchPosition(event);
+        let sliderPosition: any = getSliderPosition(touchPosition.x);
+        updateSlider(sliderPosition);
+      }
+    });
+
+    function getMousePosition(event) {
+      let clientX = event.clientX;
+      let sliderRect = slider.getBoundingClientRect();
+      let offsetX = clientX - sliderRect.left;
+      return { x: offsetX };
+    }
+
+    function getTouchPosition(event) {
+      let clientX = event.touches[0].clientX;
+      let sliderRect = slider.getBoundingClientRect();
+      let offsetX = clientX - sliderRect.left;
+      return { x: offsetX };
+    }
+
+    function getSliderPosition(offsetX) {
+      let sliderWidth = slider.offsetWidth;
+      let thumbWidth = sliderThumb.offsetWidth; // Agregamos esta línea para obtener el ancho de la bolita
+      let sliderPosition = ((offsetX - thumbWidth / 2) / (sliderWidth - thumbWidth)) * 100; // Actualizamos la posición del slider para tener en cuenta el ancho de la bolita
+      sliderPosition = Math.min(100, Math.max(0, sliderPosition)); // clamp value between 0 and 100
+      return sliderPosition;
+    }
+
+    function updateSlider(position) {
+      sliderValue = Number(position).toFixed(2);
+      sliderFill.style.width = `${sliderValue}%`;
+      sliderThumb.style.left = `calc(${sliderValue}% - 10px)`;
+      output.innerHTML = sliderValue > 0 ? sliderValue : 0;
+      output.style.left = `calc(${sliderValue}% - 30px)`;
+    }
+
   }
 
   toggleInfo() {
