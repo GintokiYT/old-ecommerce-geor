a13ac636e0a1971e640ffe3fd67f6c2eefe4ff3e
Renato <satoshiune21@gmail.com>
2023-03-29 11:34:51 -0500

    comentario xd

--- src/app/account/welcome/my-location/my-location.component.ts
+++ src/app/account/welcome/my-location/my-location.component.ts
@@ -40,6 +40,7 @@ export class MyLocationComponent extends ViewComponent implements OnInit {
 
   ngAfterViewInit(): void {
 
+    // Codigo en progreso...
     // async function checkGPS() {
     //   const { location: state } = await Geolocation.requestPermissions();
     //   // alert(state)
