864bb457a55268ed7082cfb9bee96def6b98f379
yanci97 <yanciv2005@gmail.com>
2023-03-30 10:38:01 -0500

    map

--- src/app/account/welcome/my-location/my-location.component.ts
+++ src/app/account/welcome/my-location/my-location.component.ts
@@ -482,26 +482,13 @@ export class MyLocationComponent extends ViewComponent implements OnInit {
   }
 
   nextProyect() {
-    // if (this.previousRoute.includes("manage")) {
-    //   this.navigation.back(this.previousRoute);
-    // } else {
-    //   this.navigation.root('/customer/home','forward');
-    // }
-    //send y Direction
     const currentRouter = this.router.url;
     if(currentRouter==='/send/account/welcome/my-location'){
       return this.navigation.root('/customer/send-directions','forward')
     }else if(currentRouter==='/direction/account/welcome/my-location'){
       return this.navigation.root('/customer/direction','forward')
-    }else if(this.previousRoute.includes("manage")){
-      return this.navigation.back(this.previousRoute);
-    }else{
-      this.navigation.root('/customer/home','forward');
     }
-
-
-
-    //this.navigation.root('/customer/home', 'forward');
+    this.navigation.root('/customer/home', 'forward');
   }
 
   actualizar() {
