deffa3bfe9a49f04aa9671088c2aba87e0e3afcd
Renato <satoshiune21@gmail.com>
2023-03-31 12:42:16 -0500

    ayudando a jeyson 31/03/2023

--- src/app/customer/basket/collaborative-basket/collaborative-basket.component.ts
+++ src/app/customer/basket/collaborative-basket/collaborative-basket.component.ts
@@ -11,8 +11,50 @@ import { RouteService } from '../../../services/route.service';
 export class CollaborativeBasketComponent extends ViewComponent implements OnInit {
 
   statusModal: boolean;
-  mainProduct: boolean = true;
-  Product: boolean = true;
+  allChecked = false;
+
+  products:any[] = [
+    {
+      id: 1,
+      image: '/assets/collaborative-basquet/Rectangle 1600.png',
+      price: 9780,
+      name: 'Plancha de bicarbonato <br/> pulveriza zado lorem ipsum',
+      buyer: ['Gerardo Ortíz'],
+      descriptions: [
+        {
+          id: 1,
+          description: 'Mostaza, 120x150 cm, 25 mm',
+          quantity: 10503
+        }
+      ],
+      isChecked: false
+    },
+    {
+      id: 2,
+      image: '/assets/collaborative-basquet/Producto2.png',
+      price: 48.90,
+      name: 'Acrílico liso bajo',
+      buyer: ['Gerardo Ortiz','Wilfredo','Luis'],
+      descriptions: [
+        {
+          id: 1,
+          description: 'Verde jade, 120x150 cm, 25 mm',
+          quantity: 7300
+        },
+        {
+          id: 2,
+          description: 'Naraja limon, 120x150 cm, 25 mm',
+          quantity: 2
+        },
+        {
+          id: 3,
+          description: 'Gris claro, 120x150 cm, 25 mm',
+          quantity: 4
+        }
+      ],
+      isChecked: false
+    }
+  ]
 
   constructor(_injector: Injector, private inviteService: InviteService,
     private rs: RouteService) {
@@ -27,13 +69,47 @@ export class CollaborativeBasketComponent extends ViewComponent implements OnIni
     this.navigation.root('/customer/confirm-order', 'forward');
   }
 
-  onMyEvent(status: boolean) {
-    this.mainProduct = status;
-  }
-  onProductEvent(status:boolean){
-    this.Product = status;
+  modifyProducts(producto) {
+
+    this.products = producto
+
+    let contador = 0;
+
+    this.products.forEach( product => {
+      if(product.isChecked) {
+        contador++;
+      }
+    })
+
+    if(contador === this.products.length) {
+      this.allChecked = true;
+    } else {
+      this.allChecked = false;
+    }
   }
 
-  //Seleccion todos los checkbox--- agregamos [isChecked]="allChecked" al componente app-main-product
-  allChecked = false;
+  deleteProductMain(id) {
+    console.log(id)
+  }
+
+  modifyCheckProduct() {
+    const inputCheckTotal: HTMLInputElement = document.querySelector('.container-footer input[type=checkbox]')
+
+    this.allChecked = inputCheckTotal.checked
+
+    if(this.allChecked) {
+      this.products = this.products.map( product => {
+        const newProduct = product;
+        newProduct.isChecked = true;
+        return newProduct
+      });
+    } else {
+      this.products = this.products.map( product => {
+        const newProduct = product;
+        newProduct.isChecked = false;
+        return newProduct
+      })
+    }
+
+  }
 }
--- src/app/customer/basket/collaborative-basket/product/product.component.ts
+++ src/app/customer/basket/collaborative-basket/product/product.component.ts
@@ -11,64 +11,69 @@ interface Descripcions{
   styleUrls: ['./product.component.scss'],
 })
 
-  export class ProductComponent extends ViewComponent implements OnInit {
-//eliminacion
-@Output() myEvent = new EventEmitter();
- //Agregamos [checked]="isChecked" al input para marcar o desmar el checkbox
- @Input() isChecked = false;
+  export class ProductComponent implements OnInit {
+    constructor(){}
 
-    constructor(_injector: Injector) {
-      super(_injector);
-          /* this.themeService = _injector.get(AppThemeService); */
-     }
+    ngOnInit(): void {
 
-    descripcion:Descripcions[]=[
-     {
-       detail:'Verde jade, 120x150 cm, 25 mm',
-       unit:'7,300'
-     },
-
-     {
-       detail:'Naraja limon, 120x150 cm, 25 mm',
-       unit:'2'
-     },
-
-     {
-       detail:'Gris claro, 120x150 cm, 25 mm',
-       unit:'4'
-     },
-
-    ]
-
-
-
-  ngOnInit() {}
-
-  deleteDescription(event: any): void {
-    this.message.confirm('¿Eliminar este producto?','',(confirmation) => {
-      if (confirmation) {
-        const productDiv = event.target.closest('.product-info');
-         const productId = productDiv.id;
-        if (productDiv) {
-          productDiv.remove();
-        }
-      }
-    },'Eliminar','Cancelar');
-  }
-
-  deleteProduct(){
-    this.message.confirm('¿Eliminar los productos seleccionados?','',(confirmation)=>{
-      if (confirmation) {
-       const animationdelete: HTMLDivElement = document.querySelector('.animation-deletes');
-        animationdelete.classList.add('actives');
-        setTimeout(() => {
-          this.myEvent.emit(false);
-        }, 450);
     }
-    },'Eliminar','Cancelar');
-  }
-  goProductDetail(){
-   this.navigation.root('/customer/variants-product', 'forward');
-  }
+//eliminacion
+// @Output() myEvent = new EventEmitter();
+//  //Agregamos [checked]="isChecked" al input para marcar o desmar el checkbox
+//  @Input() isChecked = false;
+
+//     constructor(_injector: Injector) {
+//       super(_injector);
+//           /* this.themeService = _injector.get(AppThemeService); */
+//      }
+
+//     descripcion:Descripcions[]=[
+//      {
+//        detail:'Verde jade, 120x150 cm, 25 mm',
+//        unit:'7,300'
+//      },
+
+//      {
+//        detail:'Naraja limon, 120x150 cm, 25 mm',
+//        unit:'2'
+//      },
+
+//      {
+//        detail:'Gris claro, 120x150 cm, 25 mm',
+//        unit:'4'
+//      },
+
+//     ]
+
+
+
+//   ngOnInit() {}
+
+//   deleteDescription(event: any): void {
+//     this.message.confirm('¿Eliminar este producto?','',(confirmation) => {
+//       if (confirmation) {
+//         const productDiv = event.target.closest('.product-info');
+//          const productId = productDiv.id;
+//         if (productDiv) {
+//           productDiv.remove();
+//         }
+//       }
+//     },'Eliminar','Cancelar');
+//   }
+
+//   deleteProduct(){
+//     this.message.confirm('¿Eliminar los productos seleccionados?','',(confirmation)=>{
+//       if (confirmation) {
+//        const animationdelete: HTMLDivElement = document.querySelector('.animation-deletes');
+//         animationdelete.classList.add('actives');
+//         setTimeout(() => {
+//           this.myEvent.emit(false);
+//         }, 450);
+//     }
+//     },'Eliminar','Cancelar');
+//   }
+//   goProductDetail(){
+//    this.navigation.root('/customer/variants-product', 'forward');
+//   }
 
 }
